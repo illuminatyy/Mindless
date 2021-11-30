1f194a05748eafc5d45ea39a0e311026086840da
illuminaty <naty.brocca@gmail.com>
2021-11-30 15:33:46 -0300

    olá Pedro

---
+++ Mindless/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mindless
+//
+//  Created by Natália Brocca dos Santos on 30/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
